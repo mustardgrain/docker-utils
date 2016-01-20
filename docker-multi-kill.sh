2a07d61916c5ac1477e7e3eff0e48c11a34a10ea
Kirk True <kirk@mustardgrain.com>
2016-01-19 17:28:16 -0800

    Removing docker-multi-kill.sh and docker-machine-multi-kill.sh as they're too dangerous :)

--- docker-multi-kill.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ `docker ps -a | grep -v "CONTAINER" | wc -l ` -gt 0 ] ; then
-  docker ps -a | grep -v "CONTAINER" | awk '{print $1}' | xargs docker stop
-  docker ps -a | grep -v "CONTAINER" | awk '{print $1}' | xargs docker rm
-fi
