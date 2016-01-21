88a979f84bd13d71363ef09ecfa370674f69ae4c
Kirk True <kirk@mustardgrain.com>
2016-01-21 08:11:06 -0800

    Then immediately fixed bug in docker-machine-create-ec2-cluster.sh

--- docker-machine-create-ec2-cluster.sh
+++ docker-machine-create-ec2-cluster.sh
@@ -10,11 +10,10 @@ rm -rf $LOG_DIR
 mkdir -p $LOG_DIR
 
 for machine_name in "$@"; do
-    echo "Creating $machine_name"
-    nohup docker-machine create \
-                         --driver amazonec2 \
-                         $machine_name > $LOG_DIR/$machine_name.txt 2>&1 &
-  done
+  echo "Creating $machine_name"
+  nohup docker-machine create \
+                       --driver amazonec2 \
+                       $machine_name > $LOG_DIR/$machine_name.txt 2>&1 &
 done
 
 echo -n "Waiting for instances to be created..."
